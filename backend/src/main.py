c1d9e37d65d091173f1eca55b14b2818444d8920
oleoz1n <leopity0@gmail.com>
2025-11-16 16:47:32 -0300

    feat(backend): adds middleware CORS

--- backend/src/main.py
+++ backend/src/main.py
@@ -4,10 +4,19 @@ Main application module
 
 from api.router import api_router
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from handlers.exceptions import pydantic_validation_exception_handler
 from pydantic_core import ValidationError
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(prefix="/api", router=api_router)
 app.add_exception_handler(ValidationError, pydantic_validation_exception_handler)  # type: ignore
